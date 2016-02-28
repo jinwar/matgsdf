fa5f5882146053681a0d3ba7ad9091301a10dc96
Ge Jin <jinwar@gmail.com>
2016-02-28 10:04:36 -0600

    fix bug

--- data_download.m
+++ data_download.m
@@ -183,7 +183,7 @@ for ie=1:length(events_info)
 			new_taxis = 0:resample_delta:(length(stadata)-1)*data_delta;
 			% apply anti-alias filter
 			fN = 1/2/data_delta;
-			w_c = 1./resample_delta/fN;
+			w_c = 1./2/resample_delta/fN;
 			[b,a] = butter(2,w_c,'low');
 			stadata = filtfilt(b,a,stadata);
 			stadata = interp1(old_taxis,stadata,new_taxis,'spline');
